e4c77389b84c70f42a577b51e45fe02ea3e9b3d2
Nhat Ho <honhattan2411@gmail.com>
2020-09-11 03:22:54 -0700

    Done Landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route} from 'react-router-dom';
+import Landing from '../../teamhack/src/pages/landing/landing.component';
+import Register from '../../teamhack/src/component/register/register.component.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/register' component={Register}/>
     </div>
   );
 }
